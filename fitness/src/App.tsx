22c86cc0273d3eb359cda51ac9ed041bbeef3f34
Diana Murtazina <murka8831@yandex.ru>
2023-03-03 13:00:43 +0400

    added types(enum)

--- fitness/src/App.tsx
+++ fitness/src/App.tsx
@@ -1,8 +1,11 @@
 import { useState } from "react";
 import NavBar from "./scenes/navbar";
+import { SelectedPage } from "@/shared/types";
 
 function App() {
-  const [selectedPage, setSelectedPage] = useState<string>("home");
+  const [selectedPage, setSelectedPage] = useState<SelectedPage>(
+    SelectedPage.Home
+  );
   return (
     <div className="app bg-gray-20">
       <NavBar selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
