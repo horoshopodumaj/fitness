c274cb12938710cbf3166b1b5e77815528f379ed
Diana Murtazina <murka8831@yandex.ru>
2023-03-03 18:19:04 +0400

    added styles for home component

--- fitness/src/scenes/home/index.tsx
+++ fitness/src/scenes/home/index.tsx
@@ -18,24 +18,24 @@ const Home = ({ setSelectedPage }: Props) => {
   return (
     <section id="home" className="gap-16 bg-gray-20 py-10 md:h-full md:pb-0">
       {/** IMAGE AND MAIN HEADER*/}
-      <div>
+      <div className="mx-auto w-5/6 items-center justify-center md:flex md:h-5/6">
         {/**MAIN HEADER */}
-        <div>
+        <div className="z-10 mt-32 pr-[20px] md:basis-3/5">
           {/**HEADINGS */}
-          <div>
-            <div>
-              <div>
+          <div className="md:-mt-20 ">
+            <div className="relative">
+              <div className="before:absolute before:-top-20 before:-left-20 before:z-[-1] md:before:content-evolvetext">
                 <img src={HomePageText} alt="home-page-text" />
               </div>
             </div>
-            <p className="">
+            <p className="mt-8 text-sm">
               Unrivaled Gym. Unparalleled Training Fitness Classes. World Class
               Studios to get the Body Shapes That you Dream of.. Get Your Dream
               Body Now.
             </p>
           </div>
           {/**ACTIONS */}
-          <div>
+          <div className="mt-8 flex items-center gap-8">
             <ActionButton setSelectedPage={setSelectedPage}>
               Join Now
             </ActionButton>
@@ -49,15 +49,15 @@ const Home = ({ setSelectedPage }: Props) => {
           </div>
         </div>
         {/**IMAGE */}
-        <div>
+        <div className="flex basis-3/5 justify-center md:z-10 md:ml-40 md:mt-16 md:justify-items-end">
           <img src={HomePageGraphic} alt="home-pageGraphic" />
         </div>
       </div>
       {/**SPONSORS */}
       {isAboveMediumScreens && (
-        <div>
-          <div>
-            <div>
+        <div className="h-[150px] w-full bg-primary-100 py-10">
+          <div className="mx-auto w-5/6">
+            <div className="flex items-center justify-between gap-8">
               <img src={SponsorRedbull} alt="redbull-sponsor" />
               <img src={SponsorForbes} alt="forbes-sponsor" />
               <img src={SponsorFortune} alt="fortune-sponsor" />
